a38a00e4cfecb726b8e4071c6c43300f4256e83c
RPM <75445940+RPMozley@users.noreply.github.com>
2025-02-08 17:14:51 +0000

    Remove files
    Removing all the old files

--- run-script.sh
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/sh
-
-php converter.php
-yarnpkg run hostlist-compiler -c ~/Documents/GitHub/AdGuardSDNS-r-a-y/hostlist-compiler-config.json -o ~/Documents/GitHub/AdGuardSDNS-r-a-y/adguard-sdns.txt
-
